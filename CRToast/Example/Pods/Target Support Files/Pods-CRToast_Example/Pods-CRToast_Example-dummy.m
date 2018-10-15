2b670e4f751fd783dfa1202f0b68e2a79b894593
xingtianzhen <15261656817@126.com>
2018-10-15 11:07:54 +0800

    修改

--- CRToast/Example/Pods/Target Support Files/Pods-CRToast_Example/Pods-CRToast_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CRToast_Example : NSObject
-@end
-@implementation PodsDummy_Pods_CRToast_Example
-@end
--- CRToast/Example/Pods/Target Support Files/Pods-CRToast_Tests/Pods-CRToast_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CRToast_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_CRToast_Tests
-@end
